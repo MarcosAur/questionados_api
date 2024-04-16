25a1be0941146358a5af8eae39f9e1ba72e235ba
Maurelio <marcosaureliomendesoliveira@gmail.com>
2024-04-16 10:51:13 -0300

    [FTR] - Initial Commit

---
+++ src/main/java/com/marcosaur/questionados_api/QuestionadosApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcosaur.questionados_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionadosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosaur/questionados_api/QuestionadosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosaur.questionados_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionadosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
